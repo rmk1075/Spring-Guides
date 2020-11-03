798133ae99e870cd94f624b42226b22f13daa48b
jamie <rmk1075@gmail.com>
2020-11-03 22:46:54 +0900

    build RESTService

--- README.md
+++ README.md
@@ -2,8 +2,28 @@
 
 ## Spring-Quickstart-Guide
 
+- url: https://spring.io/quickstart
+
 - demo
+
 - @SpringBootApplication
+
 - @RestController
+
 - @GetMapping(request)
+
 - @Requstparam(value = ..., defaultValue = ...)
+
+## RESTful Web Service
+
+- url: https://spring.io/guides/gs/rest-service/
+
+- rest_service
+
+- @SpringBootApplication
+
+  - @Configuration
+
+  - @EnableAutoConfiguration
+
+  - @ComponentScan
---
+++ rest_service/src/main/java/com/example/rest_service/GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.rest_service;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    /**
+     * @GetMapping("/greeting"): HTTP GET /greeting 요청에 응답한다.
+     * @RequestParam(): value의 조건으로 입력된 값을 bind한다.
+     * 
+     * @param name
+     * @return
+     */
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}

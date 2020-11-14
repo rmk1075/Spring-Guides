2b2f3c5d0527392e27a5dbe099724e5653ac7609
jamie <rmk1075@gmail.com>
2020-11-14 23:14:23 +0900

    consuming RESTful Web Service

---
+++ cons_rest_web_service/README.md
@@ -0,0 +1,11 @@
+# Consuming a RESTful Web Service
+
+- url: <https://spring.io/guides/gs/consuming-rest/>
+
+- cons_rest_web_service
+
+- RestTemplate
+
+- @JsonIgnoreProperties
+
+- @JsonProperty
---
+++ cons_rest_web_service/src/test/java/com/example/cons_rest_web_service/ConsRestWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cons_rest_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsRestWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
